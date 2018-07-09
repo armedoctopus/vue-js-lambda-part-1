ec1c56c8319411756781aca5f11c8e47e1a3436a
Bruno Krebs <bruno.krebs@auth0.com>
2018-07-09 19:10:39 -0300

    integrating the Vue.js client with Auth0

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,8 +6,20 @@
 </template>
 
 <script>
+import * as Auth0 from 'auth0-web'
+
 export default {
-  name: 'App'
+  name: 'App',
+  created () {
+    Auth0.configure({
+      domain: 'alien-go-home.auth0.com',
+      clientID: 'NQipGcpZsGn0CEpJPbVFoKZDY0I7WuVP',
+      audience: 'https://alien-go-home.auth0.com/userinfo',
+      redirectUri: 'http://localhost:8080/callback',
+      responseType: 'token id_token',
+      scope: 'openid profile'
+    })
+  }
 }
 </script>
 
